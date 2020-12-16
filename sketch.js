ca0f1c8b9a2391539205b0bd024f551faee1ffcf
ashrithaaadibhatla <ashrithaaadibhatla@gmail.com>
2020-12-15 19:49:04 -0600

    added platform for C-26

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground(135,300,300,170);
+
+
+
 }
 
 function draw(){
@@ -59,4 +64,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
